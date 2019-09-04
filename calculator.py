e156560ff4ea727b569a41bc283afe76fadedb85
administrator <santoshprasad0191@gmail.com>
2019-09-05 01:31:30 +0530

    Add unit tests for calculator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,12 @@
 """
+
 Calculator library containing basic Math operations.
 """
+
+
 def add(first_term, second_term):
     return first_term + second_term
+
+
 def subtract(first_term, second_term):
     return first_term - second_term
